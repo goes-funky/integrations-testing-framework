5c36919964643556a4f3b18ba6a85b3707f21763
Geri Shehu <geri.shehu@y42.com>
2022-01-11 08:50:05 +0100

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="integrations-testing-framework",
     version="0.1.0",
     description="",
     author="y42",
-    classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["integrations_testing_framework"],
     install_requires=[
         # NB: Pin these to a more specific version for tap reliability
         "requests==2.26.0",
         "responses==0.17.0",
     ],
-    packages=["integrations_testing_framework"],
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="integrations testing framework",
 )
