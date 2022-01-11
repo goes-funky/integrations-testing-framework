e85c056da017c323b38786a0fb239794656576e6
Geri Shehu <geri.shehu@y42.com>
2022-01-11 08:40:29 +0100

    Correcting package name

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     description="",
     author="y42",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["integrations_testing_module"],
+    py_modules=["integrations_testing_framework"],
     install_requires=[
         # NB: Pin these to a more specific version for tap reliability
         "requests==2.26.0",
         "responses==0.17.0",
     ],
-    packages=["integrations_testing_module"],
+    packages=["integrations_testing_framework"],
 )
