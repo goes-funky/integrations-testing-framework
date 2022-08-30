e5f9e8e2c9153bf5a5f8c4b84353f4bf6b13f410
y42sehar <94365368+y42sehar@users.noreply.github.com>
2022-08-30 11:22:05 +0200

    'add new feature to select each property metadata in select_schema'

--- integrations_testing_framework/utils.py
+++ integrations_testing_framework/utils.py
@@ -15,7 +15,7 @@ def assert_matching_file_contents(file_1, file_2):
         raise Exception("File is empty.")
 
         
-def select_schema(catalog_path: str, stream_name: str, stream_key: str = 'name') -> str:
+def select_schema(catalog_path: str, stream_name: str, stream_key: str = 'name', select_by_property = None) -> str:
     """
     Creates a catalog file with the stream_name selected.
     Example: @with_sys_args(['--config', config_path, '--catalog', utils.select_schema('all-streams.json', 'epics')])
@@ -23,6 +23,7 @@ def select_schema(catalog_path: str, stream_name: str, stream_key: str = 'name')
     :param str catalog_path: a path of a catalog
     :param str stream_name: the name of the stream to be modified
     :param str stream_key: the key that is using for stream dict to select the desired stream
+    :param select_by_property : True , when need to select : true each property metadata
     :return: the path of the catalog created as string
     :rtype: str
     """
@@ -32,9 +33,19 @@ def select_schema(catalog_path: str, stream_name: str, stream_key: str = 'name')
 
     catalog = json.loads(catalog)
 
-    for stream in catalog['streams']:
-        if stream[stream_key] == stream_name:
-            stream['metadata'][0]['metadata']['selected'] = True
+    #Selected : True added on each property metadata
+    # Add select_property = True as parameter
+    if select_by_property is True:
+        for stream in catalog['streams']:
+            if stream[stream_key] == stream_name:
+                for property in stream['metadata']:
+                    property['metadata']['selected'] = True
+
+    #Selected: True added on the metadata of the stream
+    else:
+        for stream in catalog['streams']:
+            if stream[stream_key] == stream_name:
+                stream['metadata'][0]['metadata']['selected'] = True
 
     # Open the file for writing.
     f = open(f"tests/catalogs/{stream_name}.json", "w")
