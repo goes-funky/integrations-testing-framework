349611d13195268e6e708e88207e17525a2c57ef
M Mohsin Khan <muhammad.khan@y42.com>
2022-09-17 21:32:19 +0500

    Added multiple enhancements.

--- integrations_testing_framework/decorators/http_mocking_decorators.py
+++ integrations_testing_framework/decorators/http_mocking_decorators.py
@@ -1,19 +1,66 @@
+import sys
 import vcr
-from functools import wraps
+import json
+import logging
+from functools import wraps, partial
 from pathlib import Path
 
+LOGGER = logging.getLogger()
+LOGGER.addHandler(logging.StreamHandler(sys.stderr))
 
-def intercept_requests(file_uri: str, generate=False):
+# Default match
+_MATCH_ON = {'method', 'scheme', 'host', 'port', 'path', 'query', 'body'}
+# Preserve characters during string anonymization
+_ANON_PRESERVE_CHARS = set(r'!@#$%^&*_-+=()[]{}\/<>,.?')
+
+
+def intercept_requests(file_uri: str, generate=False, ignore_on_match=None, replace_req_headers=['authorization'],
+                       replace_req_params=None, replace_req_data=None, anonymize_resp_data=False,
+                       anonymize_resp_data_skip_keys=None):
     """
-    A decorator that will intercept HTTP calls made through the requests module and depending on the
-    supplied configuration will either save the call data to a file or will mock the request with data
-    from the given file.
+    A decorator that will intercept HTTP calls and depending on the supplied configuration will either save the call
+    data to a file or will mock the request with data from the given file.
 
-    Each HTTP call is represented by an instance of the HttpRequest class.
-    Each line in written/read from the file contains a serialized HttpCall object for each request.
+    :param str file_uri: file path for storing/mocking requests.
+    :param bool generate: True: Record requests to the file, False: Mock request from data present in the file.
+    :param list ignore_on_match: Ignore attributes of the request while matching with recorded requests.
+        Possible values:
+            'method', 'scheme', 'host', 'port', 'path', 'query', 'body'
+        Examples:
+            Ignore query parameters and body while matching request
+            ignore_on_match=['query', 'body']
+        Note: If there are multiple matches of a request in the file, first match that has not already been played would
+        be selected.
+    :param list replace_req_headers: List of request headers to be replaced with dummy values before saving/matching request to/from the file.
+        Example:
+            Replace 'client-id' and 'client-secret' headers value
+            replace_req_headers=['client-id', 'client-secret']
+    :param list replace_req_params: List of request query parameters to be replaced with dummy values before saving/matching request to/from the file.
+        Example:
+            Replace 'client-id' and 'client-secret' values in query parameters
+            replace_req_params=['client-id', 'client-secret']
+    :param list replace_req_data: List of request post body attributes to be replaced with dummy values before saving/matching request to/from the file.
+        Example:
+            Replace 'access_token' value in request body
+            replace_req_data=['access_token']
+        Note: Only works for POST method.
+              Might not work well for content-types other than JSON.
+    :param bool anonymize_resp_data: Anonymize response body before saving to file.
+        Note: Only works for content-type JSON at the moment.
+              Decorated method would still receive actual response.
+    :param list anonymize_resp_data_skip_keys: List of attributes to be preserved while anonymizing response body.
+        Example:
+            Don't anonymize value of 'id' and 'sync_timestamp' fields in response body
+            anonymize_resp_data_skip_keys=['id', 'sync_timestamp']
     """
-
     record_mode = 'all' if generate else 'none'
+    match_on = _MATCH_ON - set(ignore_on_match) if ignore_on_match else _MATCH_ON
+    match_on = list(match_on)
+    replace_req_data = _to_list_of_tuple(replace_req_data or [])
+    replace_req_params = _to_list_of_tuple(replace_req_params or [])
+    replace_req_headers = _to_list_of_tuple(replace_req_headers or [])
+    kwargs = {'anon_data_skip_keys': anonymize_resp_data_skip_keys}
+    before_record_response = partial(_before_record_response, **kwargs) if anonymize_resp_data and generate else None
 
     def decorator(func):
         @wraps(func)
@@ -23,9 +70,12 @@ def intercept_requests(file_uri: str, generate=False):
                 Path(file_uri).unlink(missing_ok=True)
             with vcr.use_cassette(file_uri,
                                   record_mode=record_mode,
-                                  filter_headers=['authorization'],
+                                  filter_headers=replace_req_headers,
+                                  filter_query_parameters=replace_req_params,
+                                  filter_post_data_parameters=replace_req_data,
+                                  before_record_response=before_record_response,
                                   decode_compressed_response=True,
-                                  match_on=['method', 'scheme', 'host', 'port', 'path', 'query', 'body']) as cass:
+                                  match_on=match_on) as cass:
                 if generate is False:
                     cass.allow_playback_repeats = False
                 func(*args, **kwargs)
@@ -34,3 +84,87 @@ def intercept_requests(file_uri: str, generate=False):
         return interceptor
 
     return decorator
+
+
+def _before_record_response(response, **kwargs):
+    """
+    Callback for processing response before recording to file.
+    """
+    # Supported content-types for response body anonymization
+    anon_supported_content_types = {
+        'application/json': _anonymize_json
+    }
+    # Anonymize response body, currently works only for JSON
+    body = response['body']['string']
+    # Read content-type
+    content_type = _get_content_type(response)
+    if content_type not in anon_supported_content_types:
+        LOGGER.warning('Unsupported content-type "%s" for anonymization', content_type)
+        return response
+    anon_function = anon_supported_content_types[content_type]
+    try:
+        anon_body = anon_function(body, skip_keys=kwargs.get('anon_data_skip_keys'))
+    except ValueError as err:
+        raise ValueError('Failed to anonymize response body') from err
+    response['body']['string'] = anon_body
+    return response
+
+
+def _get_content_type(response):
+    """
+    Get content type from response header.
+    :param dict response: Response object.
+    :return: Content type.
+    :type: string
+    """
+    if 'headers' not in response:
+        return None
+    content_type = response['headers'].get('Content-Type') or response['headers'].get('content-type')
+    if not content_type or not isinstance(content_type, list):
+        return None
+    content_type = content_type[0]
+    content_type = content_type.split(';')[0]
+    return content_type.lower().strip()
+
+
+def _anonymize_json(data, skip_keys=None):
+    """
+    Anonymize JSON.
+    :param string data: JSON data.
+    :param list skip_keys: Keys to be skiped.
+    :return: Anonymized JSON.
+    :type: string
+    :raises ValueError: for invalid JSON.
+    """
+    skip_keys = set(skip_keys) if skip_keys else {}
+
+    def anonymize_data(_data):
+        # Simple types
+        if _data is None:
+            return None
+        # Invert bool value
+        if isinstance(_data, bool):
+            return not _data
+        # Add 1 to numbers
+        if isinstance(_data, (int, float)):
+            return _data + 1
+        # Replace characters in string with 'X'
+        if isinstance(_data, str):
+            return ''.join([x if x in _ANON_PRESERVE_CHARS else 'X' for x in _data])
+        # Complex types
+        if isinstance(_data, list):
+            return [anonymize_data(item) for item in _data]
+        if isinstance(_data, dict):
+            return {key: value if key in skip_keys else anonymize_data(value) for key, value in _data.items()}
+    json_data = json.loads(data)
+    return json.dumps(anonymize_data(json_data), indent=4)
+
+
+def _to_list_of_tuple(list_of_strings):
+    """
+    Covert list of strings to list of tuples.
+    :param list list_of_strings: List of strings.
+    :return: List of (string, XXXX) tuples.
+    :type: list
+    """
+    return [(string, ''.join(['X' for _ in range(len(string))])) for string in list_of_strings]
--- tests/vcr_interceptor_test.py
+++ tests/vcr_interceptor_test.py
@@ -4,29 +4,30 @@ from vcr.errors import CannotOverwriteExistingCassetteException
 
 from integrations_testing_framework.decorators.http_mocking_decorators import intercept_requests
 import pytest
+import requests
 
 
-@intercept_requests('cassette', generate=False)
+@intercept_requests('tests/cassette', generate=False)
 def test_request():
     response = urllib.request.urlopen('http://www.iana.org/domains/reserved').read()
     assert 'domains' in response.decode('utf8')
 
 
 def test_auth_removal():
-    @intercept_requests('cassette2', generate=True)
+    @intercept_requests('tests/cassette2', generate=True)
     def test_auth_request():
         request = urllib.request.Request('http://www.iana.org/domains/reserved', method='GET')
         request.add_header("Authorization", "Bearer " + 'some Test token');
         response = urllib.request.urlopen(request).read()
         assert 'domains' in response.decode('utf8')
     test_auth_request()
-    with open('cassette2', 'r') as cassette2:
+    with open('tests/cassette2', 'r') as cassette2:
         text = cassette2.read()
         assert 'some Test token' not in text
 
 
 def test_too_few_requests():
-    @intercept_requests('cassette3', generate=False)
+    @intercept_requests('tests/cassette3', generate=False)
     def test_request_cassette_with_two_requests():
         response = urllib.request.urlopen('http://www.iana.org/domains/reserved').read()
         assert 'domains' in response.decode('utf8')
@@ -37,7 +38,7 @@ def test_too_few_requests():
 
 
 def test_too_many_requests():
-    @intercept_requests('cassette3', generate=False)
+    @intercept_requests('tests/cassette3', generate=False)
     def test_three_request_for_cassette_with_two_requests():
         urllib.request.urlopen('http://www.iana.org/domains/reserved').read()
         urllib.request.urlopen('http://www.iana.org/domains/reserved').read()
@@ -45,3 +46,98 @@ def test_too_many_requests():
 
     with pytest.raises(CannotOverwriteExistingCassetteException):
         test_three_request_for_cassette_with_two_requests()
+
+
+def test_ignore_on_match():
+    """
+    Test 'ignore_on_match'.
+    """
+    @intercept_requests('tests/test_cassette', generate=True)
+    def actual_request():
+        requests.get('http://httpbin.org/anything',
+                     params={'param1': 'value1'})
+
+    @intercept_requests('tests/test_cassette', generate=False, ignore_on_match=['query'])
+    def mocked_request():
+        requests.get('http://httpbin.org/anything',
+                     params={'param1': 'value1_new'})
+
+    actual_request()
+    mocked_request()
+
+
+def test_replace_request_params():
+    """
+    Test 'replace_req_params'.
+    """
+    @intercept_requests('tests/test_cassette', generate=True, replace_req_params=['param1'])
+    def actual_request():
+        requests.get('http://httpbin.org/anything',
+                     params={'param1': 'value1'})
+
+    @intercept_requests('tests/test_cassette', generate=False, replace_req_params=['param1'])
+    def mocked_request():
+        requests.get('http://httpbin.org/anything',
+                     params={'param1': 'value123'})
+
+    actual_request()
+    mocked_request()
+
+
+def test_replace_request_data():
+    """
+    Test 'replace_req_data'.
+    """
+    @intercept_requests('tests/test_cassette', generate=True, replace_req_data=['key1'])
+    def actual_request():
+        requests.post('http://httpbin.org/anything',
+                      json={'key1': 'value1'})
+
+    @intercept_requests('tests/test_cassette', generate=False, replace_req_data=['key1'])
+    def mocked_request():
+        requests.post('http://httpbin.org/anything',
+                      json={'key1': 'value123'})
+
+    actual_request()
+    mocked_request()
+
+
+def test_anonymize_response_data():
+    """
+    Test 'anonymize_resp_data'.
+    """
+    @intercept_requests('tests/test_cassette', generate=True, anonymize_resp_data=True)
+    def actual_request():
+        requests.post('http://httpbin.org/anything',
+                      json={'key1': 'value1'})
+
+    @intercept_requests('tests/test_cassette', generate=False)
+    def mocked_request():
+        response = requests.post('http://httpbin.org/anything',
+                                 json={'key1': 'value1'})
+        assert response.json()['json']['key1'] != 'value1'
+
+    actual_request()
+    mocked_request()
+
+
+def test_anonymize_response_data_skip_keys():
+    """
+    Test 'anonymize_resp_data_skip_keys'.
+    """
+    @intercept_requests('tests/test_cassette',
+                        generate=True,
+                        anonymize_resp_data=True,
+                        anonymize_resp_data_skip_keys=['key1'])
+    def actual_request():
+        requests.post('http://httpbin.org/anything',
+                      json={'key1': 'value1'})
+
+    @intercept_requests('tests/test_cassette', generate=False)
+    def mocked_request():
+        response = requests.post('http://httpbin.org/anything',
+                                 json={'key1': 'value1'})
+        assert response.json()['json']['key1'] == 'value1'
+
+    actual_request()
+    mocked_request()
