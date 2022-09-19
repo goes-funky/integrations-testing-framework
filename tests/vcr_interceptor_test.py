2a1e444d4d55336fecbf53918c7e79e196554c5e
M Mohsin Khan <muhammad.khan@y42.com>
2022-09-19 19:25:36 +0500

    Replaced http calls with https in tests and added timeout for request.

--- tests/vcr_interceptor_test.py
+++ tests/vcr_interceptor_test.py
@@ -9,14 +9,14 @@ import requests
 
 @intercept_requests('tests/cassette', generate=False)
 def test_request():
-    response = urllib.request.urlopen('http://www.iana.org/domains/reserved').read()
+    response = urllib.request.urlopen('https://www.iana.org/domains/reserved').read()
     assert 'domains' in response.decode('utf8')
 
 
 def test_auth_removal():
     @intercept_requests('tests/cassette2', generate=True)
     def test_auth_request():
-        request = urllib.request.Request('http://www.iana.org/domains/reserved', method='GET')
+        request = urllib.request.Request('https://www.iana.org/domains/reserved', method='GET')
         request.add_header("Authorization", "Bearer " + 'some Test token');
         response = urllib.request.urlopen(request).read()
         assert 'domains' in response.decode('utf8')
@@ -29,7 +29,7 @@ def test_auth_removal():
 def test_too_few_requests():
     @intercept_requests('tests/cassette3', generate=False)
     def test_request_cassette_with_two_requests():
-        response = urllib.request.urlopen('http://www.iana.org/domains/reserved').read()
+        response = urllib.request.urlopen('https://www.iana.org/domains/reserved').read()
         assert 'domains' in response.decode('utf8')
 
     with pytest.raises(AssertionError) as err:
@@ -40,9 +40,9 @@ def test_too_few_requests():
 def test_too_many_requests():
     @intercept_requests('tests/cassette3', generate=False)
     def test_three_request_for_cassette_with_two_requests():
-        urllib.request.urlopen('http://www.iana.org/domains/reserved').read()
-        urllib.request.urlopen('http://www.iana.org/domains/reserved').read()
-        urllib.request.urlopen('http://www.iana.org/domains/reserved').read()
+        urllib.request.urlopen('https://www.iana.org/domains/reserved').read()
+        urllib.request.urlopen('https://www.iana.org/domains/reserved').read()
+        urllib.request.urlopen('https://www.iana.org/domains/reserved').read()
 
     with pytest.raises(CannotOverwriteExistingCassetteException):
         test_three_request_for_cassette_with_two_requests()
@@ -54,13 +54,15 @@ def test_ignore_on_match():
     """
     @intercept_requests('tests/test_cassette', generate=True)
     def actual_request():
-        requests.get('http://httpbin.org/anything',
-                     params={'param1': 'value1'})
+        requests.get('https://httpbin.org/anything',
+                     params={'param1': 'value1'},
+                     timeout=10)
 
     @intercept_requests('tests/test_cassette', generate=False, ignore_on_match=['query'])
     def mocked_request():
-        requests.get('http://httpbin.org/anything',
-                     params={'param1': 'value1_new'})
+        requests.get('https://httpbin.org/anything',
+                     params={'param1': 'value1_new'},
+                     timeout=10)
 
     actual_request()
     mocked_request()
@@ -72,13 +74,15 @@ def test_filter_request_params():
     """
     @intercept_requests('tests/test_cassette', generate=True, filter_req_params=['param1'])
     def actual_request():
-        requests.get('http://httpbin.org/anything',
-                     params={'param1': 'value1'})
+        requests.get('https://httpbin.org/anything',
+                     params={'param1': 'value1'},
+                     timeout=10)
 
     @intercept_requests('tests/test_cassette', generate=False, filter_req_params=['param1'])
     def mocked_request():
-        requests.get('http://httpbin.org/anything',
-                     params={'param1': 'value123'})
+        requests.get('https://httpbin.org/anything',
+                     params={'param1': 'value123'},
+                     timeout=10)
 
     actual_request()
     mocked_request()
@@ -90,13 +94,15 @@ def test_filter_request_data():
     """
     @intercept_requests('tests/test_cassette', generate=True, filter_req_data=['key1'])
     def actual_request():
-        requests.post('http://httpbin.org/anything',
-                      json={'key1': 'value1'})
+        requests.post('https://httpbin.org/anything',
+                      json={'key1': 'value1'},
+                      timeout=10)
 
     @intercept_requests('tests/test_cassette', generate=False, filter_req_data=['key1'])
     def mocked_request():
-        requests.post('http://httpbin.org/anything',
-                      json={'key1': 'value123'})
+        requests.post('https://httpbin.org/anything',
+                      json={'key1': 'value123'},
+                      timeout=10)
 
     actual_request()
     mocked_request()
@@ -108,13 +114,15 @@ def test_filter_response_data():
     """
     @intercept_requests('tests/test_cassette', generate=True, filter_resp_data=True)
     def actual_request():
-        requests.post('http://httpbin.org/anything',
-                      json={'key1': 'value1'})
+        requests.post('https://httpbin.org/anything',
+                      json={'key1': 'value1'},
+                      timeout=10)
 
     @intercept_requests('tests/test_cassette', generate=False)
     def mocked_request():
-        response = requests.post('http://httpbin.org/anything',
-                                 json={'key1': 'value1'})
+        response = requests.post('https://httpbin.org/anything',
+                                 json={'key1': 'value1'},
+                                 timeout=10)
         assert response.json()['json']['key1'] != 'value1'
 
     actual_request()
@@ -130,13 +138,15 @@ def test_filter_response_data_skip_keys():
                         filter_resp_data=True,
                         filter_resp_data_skip_keys=['key1'])
     def actual_request():
-        requests.post('http://httpbin.org/anything',
-                      json={'key1': 'value1'})
+        requests.post('https://httpbin.org/anything',
+                      json={'key1': 'value1'},
+                      timeout=10)
 
     @intercept_requests('tests/test_cassette', generate=False)
     def mocked_request():
-        response = requests.post('http://httpbin.org/anything',
-                                 json={'key1': 'value1'})
+        response = requests.post('https://httpbin.org/anything',
+                                 json={'key1': 'value1'},
+                                 timeout=10)
         assert response.json()['json']['key1'] == 'value1'
 
     actual_request()
